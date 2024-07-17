5ab07972b6493ba62e4498525eb363dc80c3cec5
shawn <shawn@rsatechnologyservices.com>
2024-07-16 22:01:37 -0500

    Code to a stable ,workable state

--- js/site.js
+++ js/site.js
@@ -1,12 +1,83 @@
+//Function 1 getValues
+//Gather the values from the user strings(inputs).
 function getValues(){
- alert("Hello App!!!");
+      
+    let inputPali = document.getElementById("paliMessage").value;     
+    
+    let reversePali = reverseString(inputPali);
+
+    let paliCheck = specialCheck(inputPali);
+
+    let paliCheckRev = specialCheck(reversePali);
+    
+    let isPalindrome = checkForPalindrome(paliCheck, paliCheckRev);
+
+    displayResults(inputPali, paliCheckRev, isPalindrome);
+
+    
 }
 
-//get values from the screen and display them
-function getValues2(){
-    let inputMessage = document.getElementById("message").value; 
+//Function 2 checkForPalindrome
+//Determine whether a string is a palindrome.
+function checkForPalindrome(Pali, reversePali){    
 
-    let alertMessage = document.getElementById("alertMessage");
+   //Comparison to check if they are the same. 
+   if(Pali == reversePali)
+    {
+        return true; 
+    }
+    else 
+    {
+      return false;  
+    }
+   
+}
+//Function 3 specialCheck
+//Convert "paliMessage".value to lowercase and eliminate whitespace and special characters.
+function specialCheck(checkPali)
+{
+    //To Lower Case
+     checkPali = checkPali.toLowerCase();
 
-    alertMessage.innerText = inputMessage;
+    //remove spaces and special characters.
+    let regex = /[^a-z0-9]/gi;
+
+    checkPali = checkPali.replace(regex,"");
+
+    return checkPali;
+
+}
+
+//Function 3 Reverse a String logic/Calculation
+function reverseString(Pali){
+
+    let reversePali = "";
+
+    for( index = Pali.length-1; index>=0; index-- ) {
+       reversePali += Pali[index];
+    }
+
+    return reversePali;
+}
+
+//Function 4 displayResults
+//Display a message to the user to show whether their string is a palindrome or not.
+function displayResults(inputPali, reversePali, isPalindrome){   
+
+    if(isPalindrome == true){
+
+        let paliAlert = document.getElementById("paliAlert");
+
+        //Success Message to User
+        paliAlert.innerHTML = `<div">Well Done! You Entered a Palindrome</div><hr>
+        <div>Your phrase reversed is: ${reversePali}</div>`;
+
+    }else{
+        
+        let paliAlertBad = document.getElementById("paliAlertBad");
+
+        //Failure Message to User
+        paliAlertBad.innerHTML = `<div>Sorry! You did not enter a Palindrome</div><hr> <div>Your phrase reversed is: ${reversePali}</div>`;
+
+    }   
 }
